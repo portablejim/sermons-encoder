fa3cb09ef6d9c99f21e3efebab5227c14b58afca
James McKay <dcseee-github@yahoo.com.au>
2014-06-22 16:28:33 +1000

    Reformat file

--- encoder.py
+++ encoder.py
@@ -12,6 +12,7 @@ class sermonsLabel(ttk.Label):
         ttk.Label.__init__(self, parent, text=text)
         self.grid(column=0, row=row, sticky=(W), padx=4)
 
+
 class EncoderUi(Frame):
     def __init__(self, model):
         self.root = Tk()
@@ -22,11 +23,11 @@ class EncoderUi(Frame):
         self.root.title("Sermon Encoder")
 
         self.parent = Frame(self.root)
-        self.parent.grid(column=0, row=0, sticky=(N,S,E,W))
+        self.parent.grid(column=0, row=0, sticky=(N, S, E, W))
         self.parent.columnconfigure(0, weight=0)
 
         self.model = model
-        self.controller=None
+        self.controller = None
 
         self.actionSermonSelected = None
 
@@ -37,19 +38,19 @@ class EncoderUi(Frame):
         self.initUI()
 
     def initUI(self):
-        self.grid(column=0, row=0, sticky=(N,S,E,W))
+        self.grid(column=0, row=0, sticky=(N, S, E, W))
         self.columnconfigure(0, weight=0)
         self.columnconfigure(1, weight=1)
 
         self.generateFilechooser()
 
         separator1 = ttk.Separator(self, orient=HORIZONTAL)
-        separator1.grid(column=0, row=1, columnspan=2, sticky=(E,W), pady=6)
+        separator1.grid(column=0, row=1, columnspan=2, sticky=(E, W), pady=6)
 
         self.generateFileInfoFields()
 
         separator2 = ttk.Separator(self, orient=HORIZONTAL)
-        separator2.grid(column=0, row=10, columnspan=2, sticky=(E,W), pady=6)
+        separator2.grid(column=0, row=10, columnspan=2, sticky=(E, W), pady=6)
 
         self.generateEncodingOptions()
 
@@ -70,9 +71,9 @@ class EncoderUi(Frame):
         self.fileChooserFrame.configure(padx=2)
 
         self.fileChooserText.grid(column=0, row=0, sticky=(W))
-        self.fileChooserFrame.grid(column=1, row=0, sticky=(E,W))
-        self.fileChooserEntry.grid(column=0, row=0, sticky=(E,W), padx=4)
-        self.fileChooserButton.grid(column=1, row=0, sticky=(E,W))
+        self.fileChooserFrame.grid(column=1, row=0, sticky=(E, W))
+        self.fileChooserEntry.grid(column=0, row=0, sticky=(E, W), padx=4)
+        self.fileChooserButton.grid(column=1, row=0, sticky=(E, W))
 
         self.fileChooserFrame.columnconfigure(0, weight=1)
 
@@ -93,9 +94,10 @@ class EncoderUi(Frame):
         series_list_container.columnconfigure(0, weight=1)
         series_list_container.rowconfigure(0, weight=1)
         series_list_scrollbar = Scrollbar(series_list_container)
-        series_list = Listbox(series_list_container, width=40, height=5, yscrollcommand=series_list_scrollbar.set, listvariable=self.sermonSeriesRecent)
-        series_list.grid(column=0, row=0, sticky=(N,S,E,W))
-        series_list_scrollbar.grid(column=1, row=0, sticky=(N,S,E))
+        series_list = Listbox(series_list_container, width=40, height=5, yscrollcommand=series_list_scrollbar.set,
+                              listvariable=self.sermonSeriesRecent)
+        series_list.grid(column=0, row=0, sticky=(N, S, E, W))
+        series_list_scrollbar.grid(column=1, row=0, sticky=(N, S, E))
         series_list_scrollbar.config(command=series_list.yview)
 
         series_list.bind("<<ListboxSelect>>", self.selectedSeries)
@@ -108,7 +110,8 @@ class EncoderUi(Frame):
         dateFrameContainer.columnconfigure(4, minsize=35, weight=2)
         dateFrameContainer.columnconfigure(5, weight=1)
         dateYearLabel = Label(dateFrameContainer, text="Year:")
-        dateYearSpinbox = Spinbox(dateFrameContainer, from_=date.today().year - 50, to=date.today().year + 50, textvariable=self.sermonDateYear)
+        dateYearSpinbox = Spinbox(dateFrameContainer, from_=date.today().year - 50, to=date.today().year + 50,
+                                  textvariable=self.sermonDateYear)
         self.sermonDateYear.set(date.today().year)
         dateMonthLabel = Label(dateFrameContainer, text="Month:")
         dateMonthSpinbox = Spinbox(dateFrameContainer, from_=1, to=12, textvariable=self.sermonDateMonth)
@@ -116,12 +119,12 @@ class EncoderUi(Frame):
         dateDayLabel = Label(dateFrameContainer, text="Day:")
         dateDaySpinbox = Spinbox(dateFrameContainer, from_=1, to=31, textvariable=self.sermonDateDay)
         self.sermonDateDay.set(date.today().day)
-        dateYearLabel.grid(column=0, row=0, padx=2, sticky=(E,W))
-        dateYearSpinbox.grid(column=1, row=0, sticky=(E,W))
-        dateMonthLabel.grid(column=2, row=0, padx=2, sticky=(E,W))
-        dateMonthSpinbox.grid(column=3, row=0, sticky=(E,W))
-        dateDayLabel.grid(column=4, row=0, padx=2, sticky=(E,W))
-        dateDaySpinbox.grid(column=5, row=0, sticky=(E,W))
+        dateYearLabel.grid(column=0, row=0, padx=2, sticky=(E, W))
+        dateYearSpinbox.grid(column=1, row=0, sticky=(E, W))
+        dateMonthLabel.grid(column=2, row=0, padx=2, sticky=(E, W))
+        dateMonthSpinbox.grid(column=3, row=0, sticky=(E, W))
+        dateDayLabel.grid(column=4, row=0, padx=2, sticky=(E, W))
+        dateDaySpinbox.grid(column=5, row=0, sticky=(E, W))
 
         title_entry = ttk.Entry(self, width=40, textvariable=self.sermonTitle)
         speaker_combo = ttk.Combobox(self, width=40, textvariable=self.sermonSpeaker)
@@ -134,8 +137,8 @@ class EncoderUi(Frame):
         directoryFrameContainer.rowconfigure(0, weight=1)
         directoryEntry = ttk.Entry(directoryFrameContainer, width=40, textvariable=self.sermonDirectory)
         directoryButton = ttk.Button(directoryFrameContainer, text="...", width=3, command=self.chooseDirectory)
-        directoryEntry.grid(column=0, row=0, sticky=(E,W), pady=3)
-        directoryButton.grid(column=1, row=0, sticky=(E,W), padx=3)
+        directoryEntry.grid(column=0, row=0, sticky=(E, W), pady=3)
+        directoryButton.grid(column=1, row=0, sticky=(E, W), padx=3)
 
         sermonsLabel(self, "Sermon title", 2)
         sermonsLabel(self, "Speaker", 3)
@@ -147,18 +150,20 @@ class EncoderUi(Frame):
         sermonsLabel(self, "Directory", 9)
 
         speaker_combo["values"] = ('Allan Blanch', 'Wayne Connor', 'David Ferres', 'Bryson Smith')
-        service_combo["values"] = ('(Common) - No suffix', 'a - Early Church', 'b - Morning Church', 'c - Evening church', 't - Tuesday Church')
+        service_combo["values"] = (
+        '(Common) - No suffix', 'a - Early Church', 'b - Morning Church', 'c - Evening church', 't - Tuesday Church')
         self.valuesService = ('', 'a', 'b', 'c', 't')
-        service_combo.bind("<<ComboboxSelected>>", lambda e: service_combo.set(self.valuesService[service_combo.current()]))
+        service_combo.bind("<<ComboboxSelected>>",
+                           lambda e: service_combo.set(self.valuesService[service_combo.current()]))
 
-        title_entry.grid(column=1, row=2, sticky=(E,W), pady=3)
-        speaker_combo.grid(column=1, row=3, sticky=(E,W), pady=3)
-        passage_entry.grid(column=1, row=4, sticky=(E,W), pady=3)
-        series_entry.grid(column=1, row=5, sticky=(E,W), pady=3)
-        series_list_container.grid(column=1, row=6, sticky=(N,S,E,W), pady=3)
-        service_combo.grid(column=1, row=7, sticky=(E,W), pady=3)
+        title_entry.grid(column=1, row=2, sticky=(E, W), pady=3)
+        speaker_combo.grid(column=1, row=3, sticky=(E, W), pady=3)
+        passage_entry.grid(column=1, row=4, sticky=(E, W), pady=3)
+        series_entry.grid(column=1, row=5, sticky=(E, W), pady=3)
+        series_list_container.grid(column=1, row=6, sticky=(N, S, E, W), pady=3)
+        service_combo.grid(column=1, row=7, sticky=(E, W), pady=3)
         dateFrameContainer.grid(column=1, row=8, pady=3)
-        directoryFrameContainer.grid(column=1, row=9, sticky=(E,W), pady=3)
+        directoryFrameContainer.grid(column=1, row=9, sticky=(E, W), pady=3)
 
         self.titleEntry = title_entry
         self.speakerCombo = speaker_combo
@@ -185,20 +190,22 @@ class EncoderUi(Frame):
 
         encodingOptionsLabel = ttk.Label(self, text="Enable")
         #encodeOutputLabel = ttk.Label(self, text="Suffix")
-        encode1Checkbox = ttk.Checkbutton(encodingOptionsContainer, text="Dialup MP3", variable=self.encode1, onvalue=True)
-        encode2Checkbox = ttk.Checkbutton(encodingOptionsContainer, text="High Quality MP3", variable=self.encode2, onvalue=True)
+        encode1Checkbox = ttk.Checkbutton(encodingOptionsContainer, text="Dialup MP3", variable=self.encode1,
+                                          onvalue=True)
+        encode2Checkbox = ttk.Checkbutton(encodingOptionsContainer, text="High Quality MP3", variable=self.encode2,
+                                          onvalue=True)
         encode3Checkbox = ttk.Checkbutton(encodingOptionsContainer, text="Opus", variable=self.encode3, onvalue=True)
         #encodeOutputEntry = ttk.Entry(self, textvariable=self.outputSuffix)
         encodeButton = ttk.Button(self, text="Encode")
 
         encodingOptionsLabel.grid(column=0, row=11, sticky=(W))
-        encode1Checkbox.grid(column=0, row=0, sticky=(E,W), padx=8)
-        encode2Checkbox.grid(column=1, row=0, sticky=(E,W), padx=8)
-        encode3Checkbox.grid(column=2, row=0, sticky=(E,W), padx=8)
+        encode1Checkbox.grid(column=0, row=0, sticky=(E, W), padx=8)
+        encode2Checkbox.grid(column=1, row=0, sticky=(E, W), padx=8)
+        encode3Checkbox.grid(column=2, row=0, sticky=(E, W), padx=8)
         encodingOptionsContainer.grid(column=1, row=11, pady=5)
         #encodeOutputLabel.grid(column=0, row=12, sticky=(W))
         #encodeOutputEntry.grid(column=1, row=12, sticky=(E,W))
-        encodeButton.grid(column=1, row=13, sticky=(E,W), padx = 10, pady=15)
+        encodeButton.grid(column=1, row=13, sticky=(E, W), padx=10, pady=15)
 
         self.encode1Checkbox = encode1Checkbox
         self.encode2Checkbox = encode2Checkbox
@@ -233,7 +240,8 @@ class EncoderUi(Frame):
         self.seriesList.configure(listvariable=self.sermonSeriesRecent)
 
     def chooseFile(self):
-        filename = filedialog.askopenfilename(filetypes=(("Lossless audio", "*.flac *.wav *.aiff;"),("Lossy audio", "*.opus *.ogg *.mp3")))
+        filename = filedialog.askopenfilename(
+            filetypes=(("Lossless audio", "*.flac *.wav *.aiff;"), ("Lossy audio", "*.opus *.ogg *.mp3")))
         self.targetFilename.set(filename)
 
     def chooseDirectory(self):
@@ -297,7 +305,7 @@ class EncoderUi(Frame):
 
     def selectedSeries(self, arg):
         selectedId = int(self.seriesList.curselection()[0])
-        selectedName= self.sermonSeriesRecentVar[selectedId]
+        selectedName = self.sermonSeriesRecentVar[selectedId]
         self.actionSermonSelected(selectedName)
 
     def setSeries(self, speaker, name, service, directory):
@@ -338,13 +346,15 @@ class Data:
     def insertSeries(self, name, speaker, service, directory):
         cur = self.conn.cursor()
         today = datetime.now()
-        cur.execute("INSERT INTO series(seriesName, used, speaker, service, directory) VALUES(?,?,?,?,?)", (name, today, speaker, service, directory))
+        cur.execute("INSERT INTO series(seriesName, used, speaker, service, directory) VALUES(?,?,?,?,?)",
+                    (name, today, speaker, service, directory))
         self.conn.commit()
 
     def selectSeries(self, seriesName):
         cur = self.conn.cursor()
         cur.execute("SELECT * FROM series WHERE seriesName=?", (seriesName,))
         return cur.fetchone()
+
 
 class Controller:
     def __init__(self, view, model):
@@ -353,12 +363,14 @@ class Controller:
 
     def encode(self):
         self.view.disableFields()
-        self.model.insertSeries(self.view.sermonSeries.get(), self.view.sermonSpeaker.get(), self.view.sermonService.get(), self.view.sermonDirectory.get())
+        self.model.insertSeries(self.view.sermonSeries.get(), self.view.sermonSpeaker.get(),
+                                self.view.sermonService.get(), self.view.sermonDirectory.get())
         self.view.enableFields()
 
     def seriesSelected(self, seriesName):
         seriesRecord = self.model.selectSeries(seriesName)
         self.view.setSeries(seriesRecord[2], seriesRecord[0], seriesRecord[3], seriesRecord[4])
+
 
 def main():
     data = Data()
@@ -370,6 +382,7 @@ def main():
     gui.root.mainloop()
 
     pass
+
 
 if __name__ == '__main__':
     main()
